86a9f4614e78d5a586ffb0ac3e9010054813caab
DangHuengZing <48856416+DangHuengZing@users.noreply.github.com>
2025-04-28 15:09:53 +0800

    Update urls.py

--- ai_site/urls.py
+++ ai_site/urls.py
@@ -1,8 +1,8 @@
-# ai_site/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),  # 引入 Django 自带的认证系统 URL
     path('', include('ai_api.urls')),  # 包含 ai_api 的 urls
 ]
