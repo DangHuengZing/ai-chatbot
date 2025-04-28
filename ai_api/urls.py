da1f22098810d2036f753bd9cda8056c689aa207
DangHuengZing <48856416+DangHuengZing@users.noreply.github.com>
2025-04-28 17:21:39 +0800

    Update urls.py

--- ai_api/urls.py
+++ ai_api/urls.py
@@ -1,7 +1,9 @@
 # ai_api/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('chat/stream/', views.stream_chat, name='stream_chat'),  # 流式聊天API
+    path("chat/", views.chat_page, name="chat_page"),       # 返回聊天页面
+    path("chat/stream/", views.stream_chat, name="stream_chat"),  # 流式输出
 ]
