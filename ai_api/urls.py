9dad2a0ebbda0c43e9d556511d18c5adc09c677c
DangHuengZing <48856416+DangHuengZing@users.noreply.github.com>
2025-04-29 12:37:52 +0800

    Update urls.py

--- ai_api/urls.py
+++ ai_api/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('stream_chat/', views.stream_chat_page, name='stream_chat_page'),  # 页面
     path('chat/stream/', views.stream_chat, name='stream_chat'),             # 流式聊天接口
-    path('accounts/', include('django.contrib.auth.urls')),                  # 登录、登出路由
+    path('accounts/', include('django.contrib.auth.urls')),                  # 登录登出
 ]
