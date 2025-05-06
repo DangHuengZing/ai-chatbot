6c8794f0b0600b6ee24b044b826f6d91e7041583
DangHuengZing <48856416+DangHuengZing@users.noreply.github.com>
2025-05-06 16:13:24 +0800

    Update urls.py

--- ai_api/urls.py
+++ ai_api/urls.py
@@ -1,8 +1,11 @@
+# ai_api/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
     path('stream_chat/', views.stream_chat_page, name='stream_chat_page'),
     path('chat/stream/', views.stream_chat, name='stream_chat'),
 ]
