f52784707065b09566478ce5b34580cd7b4afed0
DangHuengZing <48856416+DangHuengZing@users.noreply.github.com>
2025-05-06 16:13:43 +0800

    Update models.py

--- ai_api/models.py
+++ ai_api/models.py
@@ -1,4 +1,5 @@
 # ai_api/models.py
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -20,3 +21,6 @@ class ChatMessage(models.Model):
         indexes = [
             models.Index(fields=['user', 'model_type']),
         ]
+
+    def __str__(self):
+        return f"{self.user.username} - {self.timestamp} - {self.model_type}"
