462dcb7ea569ed8b0c30f1f76ad1172fa9594562
DangHuengZing <48856416+DangHuengZing@users.noreply.github.com>
2025-05-06 15:08:26 +0800

    Update urls.py

--- ai_api/urls.py
+++ ai_api/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_user, name='login_user'),
+    path('login/', views.login_user, name='login'),
     path('stream_chat/', views.stream_chat_page, name='stream_chat_page'),
     path('chat/stream/', views.stream_chat, name='stream_chat'),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
