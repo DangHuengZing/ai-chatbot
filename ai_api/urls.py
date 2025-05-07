965cc2a15f1746f619009e577adfa085305a88fd
DangHuengZing <48856416+DangHuengZing@users.noreply.github.com>
2025-05-07 10:29:39 +0800

    Update urls.py

--- ai_api/urls.py
+++ ai_api/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('stream_chat/', views.stream_chat_page, name='stream_chat_page'),
+    path('stream_chat/<uuid:conversation_id>/', views.stream_chat_page, name='stream_chat_conversation'),
     path('chat/stream/', views.stream_chat, name='stream_chat'),
+    path('chat/conversations/', views.get_conversations, name='get_conversations'),
 ]
