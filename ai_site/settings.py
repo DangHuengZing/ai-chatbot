b4d91cf97eddaa2a16d7acb50e26ad4ca624e516
DangHuengZing <48856416+DangHuengZing@users.noreply.github.com>
2025-05-14 15:11:31 +0800

    Update settings.py

--- ai_site/settings.py
+++ ai_site/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import os # 用于环境变量
+import os # 用于环境变量。
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
